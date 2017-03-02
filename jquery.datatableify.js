e58b94a3059d2461ce2f5c97efef08fcc545ae32
Tiger Fok <tiger.indzz@gmail.com>
2017-03-02 18:33:37 +0800

    Cleanup

--- jquery.datatableify.js
+++ jquery.datatableify.js
@@ -33,9 +33,9 @@
                     searchable: $(v).data('datatable-searchable'),
                     searchType: $(v).data('datatable-search-type'),
                     searchCustom: $(v).data('datatable-search-custom'),
+                    searchOptions: $(v).data('datatable-search-options'),
                     sortable: $(v).data('datatable-sortable'),
-                    className: $(v).data('datatable-class'),
-                    options: typeof $(v).data('datatable-options') == 'object' ? $(v).data('datatable-options') : []
+                    className: $(v).data('datatable-class')
                 });
             }
         });
@@ -77,20 +77,20 @@
         // Build Search Header
         $this.find('thead').append('<tr class="thead-search"></tr>');
         $this.find('thead th').each(function (k, v) {
-            if (cols[k]['searchable'] != false) {
-                if (typeof cols[k]['searchCustom'] != 'undefined') {
-                    $this.find('.thead-search').append('<td>' + $(cols[k]['searchCustom']).html() + '</td>');
-                } else if (typeof cols[k]['options'] == 'object' && Object.keys(cols[k]['options']).length > 0) {
+            if (cols[k]['searchable'] != false) {                    
+                if (cols[k]['searchType'] == 'select' && typeof cols[k]['searchOptions'] == 'object' && Object.keys(cols[k]['searchOptions']).length > 0) {
                     $this.find('.thead-search').append('<td><select style="width: 100%" class="simple-search-field select_' + cols[k]['data'] + '"></select></td>');
                     $this.find('.select_' + cols[k]['data']).append('<option value=""></option>');
 
-                    for (var i in cols[k]['options']) {
-                        $this.find('.select_' + cols[k]['data']).append('<option value="' + i + '">' + cols[k]['options'][i] + '</option>');
+                    for (var i in cols[k]['searchOptions']) {
+                        $this.find('.select_' + cols[k]['data']).append('<option value="' + i + '">' + cols[k]['searchOptions'][i] + '</option>');
                     }
                 } else if (cols[k]['searchType'] == 'date-range') {
                     $this.find('.thead-search').append('<td><input type="text" placeholder="From" style="width: 50%" class="date-range-search-field datepicker datatable-filter" name="' + cols[k]['data'] + '_from"/><input type="text" placeholder="To" style="width: 50%" class="date-range-search-field datepicker datatable-filter" name="' + cols[k]['data'] + '_to"/></td>');
                 } else if (cols[k]['searchType'] == 'date') {
                     $this.find('.thead-search').append('<td><input type="text" placeholder="" style="width: 100%" class="simple-search-field datepicker"/></td>');
+                } else if (cols[k]['searchType'] == 'custom') {
+                    $this.find('.thead-search').append('<td>' + $(cols[k]['searchCustom']).html() + '</td>');
                 } else {
                     $this.find('.thead-search').append('<td><input type="text" placeholder="" style="width: 100%" class="simple-search-field"/></td>');
                 }
